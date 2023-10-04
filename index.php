d3a75dfa761acbc879e8cff7ce9c4ce53afc34da
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-10-04 02:08:40 +0200

    layout sfondo completo

--- index.php
+++ index.php
@@ -5,11 +5,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/styles/style.css">
     <title>Edusogno</title>
 </head>
 
 <body>
+    <header>
+        <div class="containerNavbar" >      
+            <div class="containerLogo">
+                <img class="logo" src="./assets/img/logo.png" alt="logoEduSogno">
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="containerMain">
 
+
+
+            <div class="waveContainer">
+                    <img class="singlewave first" src="./assets/img/whitewave.svg" alt="WhiteWave">
+                    <img class="singlewave second" src="./assets/img/secondwave.svg" alt="SecondWave">
+                    <img class="singlewave third" src="./assets/img/thirdwave.svg" alt="ThirdWave">
+                    <img class="rocketElement" src="./assets/img/rocket.svg" alt="">
+                    <img class="halfCircle" src="./assets/img/halfcircle.svg" alt="">
+                    <img class="circle" src="./assets/img/circle.svg" alt="">
+            </div>
+           
+            
+          
+
+        </div>
+    </main>
 </body>
 
 </html>
